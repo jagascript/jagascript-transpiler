c12fdfca47f6d1382f34de61efb421881ce28c7d
kandros <kandros5591@gmail.com>
2016-02-03 11:42:17 +0100

    first implementation

---
+++ jagascript.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+const path = require('path')
+const args = process.argv;
+const inputfile = args[2];
+const inputfileName = path.basename(inputfile, '.jaga')
+const inputExt = getExtension(inputfile);
+
+if (inputExt !== 'jaga') {
+  console.log('the provided file is not a valid jagascript file \nPlease provide a valid ".jaga" file')
+} else {
+  fs.createReadStream(inputfile).pipe(fs.createWriteStream(`${inputfileName}.js`));
+}
+
+function getExtension(filename) {
+    var ext = path.extname(filename||'').split('.');
+    return ext[ext.length - 1];
+}
