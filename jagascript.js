5b25c0d81afadc9ec1cad6fcecd033e4b7740aeb
kandros <kandros5591@gmail.com>
2016-02-03 12:22:04 +0100

    important stuff

--- jagascript.js
+++ jagascript.js
@@ -6,10 +6,11 @@ const inputfileName = path.basename(inputfile, '.jaga')
 const inputExt = getExtension(inputfile);
 
 if (inputExt !== 'jaga') {
-  console.log('the provided file is not a valid jagascript file \nPlease provide a valid ".jaga" file')
-} else {
-  fs.createReadStream(inputfile).pipe(fs.createWriteStream(`${inputfileName}.js`));
+  console.log('the provided file is not a valid jagascript file \nPlease provide a valid ".jaga" file');
+  return;
 }
+
+fs.createReadStream(inputfile).pipe(fs.createWriteStream(`${inputfileName}.js`));
 
 function getExtension(filename) {
     var ext = path.extname(filename||'').split('.');
